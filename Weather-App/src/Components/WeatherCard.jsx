15f8db61e8dd04719f79b798d98173594fa6abc8
hanifubaaba <baabahanif88@gmail.com>
2025-04-16 04:22:42 +0100

    set state

--- Weather-App/src/Components/WeatherCard.jsx
+++ Weather-App/src/Components/WeatherCard.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 export default function WeatherCard() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   return (
     <div className="weather-container">
       <h1 className="weather-title">Weather Info</h1>
-      <input type="text" className="city-input" placeholder="Enter City..." />
+      <input
+        type="text"
+        className="city-input"
+        placeholder="Enter City..."
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
       <button className="weather-btn">Get Weather Data</button>
       <div className="weather-info">Weather Data</div>
     </div>
