f31971cf9461138dddf1b8f93f71cf30086078df
hanifubaaba <baabahanif88@gmail.com>
2025-04-16 05:32:05 +0100

    created function for fetching data, added error handling

--- Weather-App/src/Components/WeatherCard.jsx
+++ Weather-App/src/Components/WeatherCard.jsx
@@ -1,10 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function WeatherCard() {
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+
+  const API_KEY = "d0e248396fe4189a3c93cfb6ca0c881d";
+  async function fetchWeatherData() {
+    if (!city.trim()) {
+      setError("Please enter a city name");
+      return;
+    }
+    setLoading(true);
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+      );
+      if (!res.ok) {
+        throw new Error("Invalid City Name or Request Failed");
+      }
+      const data = await res.json();
+      setWeatherData(data);
+      setError(null);
+    } catch (error) {
+      setError("Sorry Couldn't get weather data");
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
     <div className="weather-container">
       <h1 className="weather-title">Weather Info</h1>
@@ -15,7 +39,9 @@ export default function WeatherCard() {
         value={city}
         onChange={(e) => setCity(e.target.value)}
       />
-      <button className="weather-btn">Get Weather Data</button>
+      <button className="weather-btn" onClick={fetchWeatherData}>
+        Get Weather Data
+      </button>
       <div className="weather-info">Weather Data</div>
     </div>
   );
